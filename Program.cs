cb0734ebe5953cfd5182a7553b06637bcb6d9a96
Wade Koehmstedt <wkoehmstedt@gmail.com>
2019-04-07 22:24:29 -0700

    Commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace hello_world
+{
+    class Program
+    {
+        static int owen(int x)
+        {
+            return x+100;
+        }
+
+        static void Main(string[] args)
+        {
+            string sometext = "oopopopopoopopopop";
+            Console.WriteLine(sometext);
+            double d = 1.2223232323232;
+            string s = "text";
+            float f = 1.1F;
+            long l = 123456789012345;
+            short sh = 255;
+            byte b = 8;
+
+            for (int i=10; i<1000; i+=10)
+            {
+                int o = owen(i);
+                Console.WriteLine(o);
+            }
+
+            Console.WriteLine(d);
+        }
+
+
+    }
+}
